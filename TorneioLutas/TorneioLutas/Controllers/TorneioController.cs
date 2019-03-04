87c5f592efa3fd3e61203bb5d15c8293ab094f8f
Adan Paulo Ferrarez Fabbri <adan.paulo@gmail.com>
2019-03-04 15:08:18 -0300

    desenv

--- TorneioLutas/TorneioLutas/Controllers/TorneioController.cs
+++ TorneioLutas/TorneioLutas/Controllers/TorneioController.cs
@@ -1,17 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using TorneioLutas.Models;
 
 namespace TorneioLutas.Controllers
 {
     public class TorneioController : Controller
     {
+
+        #region Actions
+
         // GET: Torneio
         public ActionResult Index()
         {
-            return View();
+            try
+            {
+                var listaLutadores = retornaLutadores();
+
+                return View(listaLutadores);
+            }
+            catch (Exception ex)
+            {
+                TempData["Msg"] = "Erro ao buscar competidores. Erro: " + ex.Message;
+                return View();
+            }
+
+            
         }
+
+        #endregion
+
+        #region Métodos
+
+        private List<Lutador> retornaLutadores()
+        {
+            try
+            {
+                List<Lutador> oListLutador = new List<Lutador>();
+
+                using (var client = new WebClient())
+                {
+                    var json = client.DownloadString("http://177.36.237.87/lutadores/api/competidores");
+
+                    byte[] bytes = Encoding.Default.GetBytes(json);
+                    json = Encoding.UTF8.GetString(bytes);
+
+                    var serializer = new JavaScriptSerializer();
+                    oListLutador = serializer.Deserialize<List<Lutador>>(json);
+                }
+
+                return oListLutador;
+            }
+            catch (Exception ex)
+            {
+                return null;
+                throw ex;
+            }
+        }
+
+        #endregion
+
     }
 }
---
+++ TorneioLutas/TorneioLutas/Models/Lutador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TorneioLutas.Models
+{
+    public class Lutadores
+    {
+        public List<Lutador> lutadores { get; set; }
+       
+    }
+
+    public class Lutador
+    {
+        public string nome { get; set; }
+        public int idade { get; set; }
+        public List<string> artesMarciais { get; set; }
+        public int lutas { get; set; }
+        public int derrotas { get; set; }
+        public int vitorias { get; set; }
+        public bool selecionado { get; set; }
+    }
+}
